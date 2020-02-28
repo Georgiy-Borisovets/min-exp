ffc6e398459eacf78d780b4096812cc1489fe4f4
Georgiy-Borisovets <georgiy.borisovets@gmail.com>
2020-02-28 13:46:40 +0200

    Added test service, authentication

--- services/auth/controllers/user.js
+++ services/auth/controllers/user.js
@@ -1,61 +1,26 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
+const notAuthorized = (res) => {
+    return res.status(401).send({ error: 'Not authorized to access this resource' });
+};
+
 module.exports = {
-    create(req, res, next) {
-        const user = new User(req.body);
-        return user.save()
-            .then(user => {
-                return user.generateAuthToken();
-            })
-            .then(token => {
-                return res.json({ user, token });
-            })
-            .catch(next);
-    },
-    getAll(req, res, next) {
-        const skip = 0;
-        const limit = 20;
+    auth(req, res, next) {
+        const authHeader = req.header('Authorization');
+        if (!authHeader) {
+            return notAuthorized(res);
+        }
 
-        User.find({}, null, { skip, limit }, (err, users) => {
-            if (err) {
-                next(err);
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const data = jwt.verify(token, process.env.JWT_SECRET);
+
+        User.findOne({ _id: data._id, 'tokens.token': token }, (err, user) => {
+            if (err || !user) {
+                return notAuthorized(res);
             }
 
-            res.json(users);
-        });
-    },
-    findById(req, res, next) {
-        const { id } = req.params;
-        User.findById(id, (err, user) => {
-            if (err) {
-                next(err);
-            }
-
-            res.json(user);
-        });
-    },
-    update(req, res, next) {
-        const { id } = req.params;
-        const user = req.body;
-
-        User.findOneAndUpdate({ _id: id }, user, { new: true }, (err, updatedUser) => {
-            if (err) {
-                next(err);
-            }
-
-            res.json(updatedUser);
-        });
-    },
-    delete(req, res) {
-        const { id } = req.params;
-        User.findById(id, (err, user) => {
-            if (err) {
-                res.send(err);
-            }
-
-            user.deleteOne().then(() => {
-                res.json({ message: 'ok' });
-            });
+            return res.status(200).send({ active: true });
         });
     },
     login(req, res) {
@@ -78,17 +43,78 @@ module.exports = {
                 res.status(400).send(err);
             });
     },
-    currentUser(req, res) {
-        res.json(req.user);
-    },
     logout(req, res, next) {
+        if (!req.user.tokens || !Array.isArray(req.user.tokens)) {
+            return next();
+        }
+
         req.user.tokens = req.user.tokens.filter((token) => {
-            return token.token !== req.token
+            return token.token !== req.token;
         });
         req.user.save()
             .then(() => {
-                res.send({ message: 'ok' });
+                return res.status(200).send({ message: 'ok' });
             })
             .catch(next);
     },
+    create(req, res, next) {
+        const user = new User(req.body);
+        return user.save()
+            .then(user => {
+                return user.generateAuthToken();
+            })
+            .then(token => {
+                return res.json({ user, token });
+            })
+            .catch(next);
+    },
+    // getAll(req, res, next) {
+    //     const skip = 0;
+    //     const limit = 20;
+    //
+    //     User.find({}, null, { skip, limit }, (err, users) => {
+    //         if (err) {
+    //             next(err);
+    //         }
+    //
+    //         res.json(users);
+    //     });
+    // },
+    // findById(req, res, next) {
+    //     const { id } = req.params;
+    //     User.findById(id, (err, user) => {
+    //         if (err) {
+    //             next(err);
+    //         }
+    //
+    //         res.json(user);
+    //     });
+    // },
+    // update(req, res, next) {
+    //     const { id } = req.params;
+    //     const user = req.body;
+    //
+    //     User.findOneAndUpdate({ _id: id }, user, { new: true }, (err, updatedUser) => {
+    //         if (err) {
+    //             next(err);
+    //         }
+    //
+    //         res.json(updatedUser);
+    //     });
+    // },
+    // delete(req, res) {
+    //     const { id } = req.params;
+    //     User.findById(id, (err, user) => {
+    //         if (err) {
+    //             res.send(err);
+    //         }
+    //
+    //         user.deleteOne().then(() => {
+    //             res.json({ message: 'ok' });
+    //         });
+    //     });
+    // },
+    // currentUser(req, res) {
+    //     res.json(req.user);
+    // },
 };
--- services/auth/routes/index.js
+++ services/auth/routes/index.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const router = express.Router();
-
 const userController = require('../controllers/user');
-const auth = require('../middlewares/auth.js');
 
-router.route('/users')
-    .get(userController.getAll)
-    .post(userController.create);
+// router.route('/users')
+//     .get(userController.getAll)
+//     .post(userController.create);
+//
+// router.get('/me', auth, userController.currentUser);
+// router.post('/logout', auth, userController.logout);
+//
+// router.route('/login')
+//     .post(userController.login);
+//
+// router.route('/:id')
+//     .get(userController.findById)
+//     .put(userController.update)
+//     .delete(userController.delete);
 
-router.get('/me', auth, userController.currentUser);
-router.post('/logout', auth, userController.logout);
-
-router.route('/login')
-    .post(userController.login);
-
-router.route('/:id')
-    .get(userController.findById)
-    .put(userController.update)
-    .delete(userController.delete);
+router.get('/', userController.auth);
+router.post('/signup', userController.create);
+router.post('/login', userController.login);
+router.post('/logout', userController.logout);
 
 module.exports = router;
